519298b4a7baed7e31c4862911f8bf655f9a8840
ElCapitan <gameforceman1808@gmail.com>
2023-04-09 03:25:39 +0300

    ATLB-v1.7.8_tsuberg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # by ElCapitan, PROJECT Limited 2022
-print("AT PROJECT Limited, 2022 - 2023; ATLB-v1.7.8")
+print("AT PROJECT Limited, 2022 - 2023; ATLB-v1.7.8_tsuberg\nHappy birthday, Alice Tsuberg!")
 try:
     print("\tImporting libraries...")
     import discord
