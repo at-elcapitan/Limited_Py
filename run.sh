a2de206926455fc7f39e16bad09ffdbc45b85631
ElCapitan <gameforceman1808@gmail.com>
2023-07-16 18:21:28 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,17 +4,16 @@ set -e
 echo "Executing: pip install venv"
 python3 -m pip install virtualenv
 
-echo "Checking venv/"
-[ ! -d "venv" ] && echo "venv/ not exists, creating" && python3 -m virtualenv venv
+echo "Checking venvl/"
+[ ! -d "venvl" ] && echo "venv/ not exists, creating" && python3 -m virtualenv venvl
 
 echo "Entering virtualenv"
-source ./venv/Scripts/activate
+source ./venvl/bin/activate
 
 echo "Executing: pip install -r requirements.txt"
 python3 -m pip install -r requirements.txt
 
-echo "Starting bot..."
-timeout 2s 
+echo "Starting bot..." 
 clear
 
 python3 atlb
